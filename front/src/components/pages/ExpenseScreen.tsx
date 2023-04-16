05826f02d771d585f5ee99c482867b69509e2dc5
teme-qualia <82814784+teme-qualia@users.noreply.github.com>
2023-04-17 00:28:10 +0900

    #16 家計簿入力画面のベースっぽいものを作成

--- front/src/components/pages/ExpenseScreen.tsx
+++ front/src/components/pages/ExpenseScreen.tsx
@@ -1,5 +1,16 @@
 import React, { Component } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+import AmountInput from '../molecules/AmountInout'
+import {TextArea,Box, FormControl, Input, Select, CheckIcon} from 'native-base'
+
+
+
+type HandleAmountChangeType = (amount: number) => void;
+
+const handleAmountChange: HandleAmountChangeType = (amount) => {
+  console.log(`New amount value: ${amount}`);
+  // 金額が変更されたときの処理をここに書く
+};
 
 export type Expense = {
   id: string;
@@ -46,12 +57,45 @@ export default class ExpenseForm extends Component<
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.label}>大分類</Text>
-        <Text style={styles.label}>小分類</Text>
-        <Text style={styles.label}>金額</Text>
-        <Text style={styles.label}>メモ</Text>
-        <Text style={styles.label}>日付</Text>
-        <Text style={styles.label}>購入者</Text>
+        <FormControl>
+          <FormControl.Label>大分類</FormControl.Label>
+          <Select minWidth="200" accessibilityLabel="大分類" placeholder="大分類" _selectedItem={{
+            bg: "teal.600",
+            endIcon: <CheckIcon size={5} />
+          }} mt="1">
+            <Select.Item label="大分類１" value="d1" />
+            <Select.Item label="大分類２" value="d2" />
+            <Select.Item label="大分類３" value="d3" />
+        </Select>
+        </FormControl>
+        <FormControl>
+          <FormControl.Label>小分類</FormControl.Label>
+          <Select minWidth="200" accessibilityLabel="小分類" placeholder="小分類" _selectedItem={{
+            bg: "teal.600",
+            endIcon: <CheckIcon size={5} />
+          }} mt="1">
+            <Select.Item label="小分類１" value="s1" />
+            <Select.Item label="小分類２" value="s2" />
+            <Select.Item label="小分類３" value="s3" />
+        </Select>
+        </FormControl>
+        <AmountInput
+          label="金額"
+          value={this.state.amount}
+          onChange={(value) => handleAmountChange(value)}
+        />
+        <FormControl>
+          <FormControl.Label>日付</FormControl.Label>
+          <Input variant="outline" placeholder="日付" />
+        </FormControl>
+        <FormControl>
+          <FormControl.Label>購入者</FormControl.Label>
+          <Input variant="outline" placeholder="購入者" />
+        </FormControl>
+        <FormControl>
+          <FormControl.Label>メモ</FormControl.Label>
+          <TextArea placeholder="メモ"/>
+        </FormControl>
       </View>
     );
   }
